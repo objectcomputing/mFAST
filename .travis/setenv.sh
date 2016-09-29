f20364ce85d739954fb491685807d387aaff8232
Huang-Ming Huang <huangh@ociweb.com>
2016-09-29 09:42:24 -0500

    Update .travis.yml to use matrix

---
+++ .travis/setenv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$COMPILER" == "Emscripten" ]]; then
+  CMAKE_TOOLCHAIN_FILE=`docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' thewtex/cross-compiler-browser-asmjs | grep CMAKE_TOOLCHAIN_FILE | sed 's/^.*=//'`
+  CONFIG_ARGS="-DBoost_INCLUDE_DIR=/usr/local/include -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE"
+  RUN="docker run --rm \
+                  -v $PWD:/mfast \
+                  -v /usr/include/boost:/usr/local/include/boost \
+                  -e "AR="
+                  -e "CXX="
+                  -t thewtex/cross-compiler-browser-asmjs "
+else
+  CONFIG_ARGS="-DCMAKE_CXX_COMPILER=$COMPILER"
+fi                  
