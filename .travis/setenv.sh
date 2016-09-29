7ead35bc85a20a70340221371125a15676342156
Huang-Ming Huang <huangh@ociweb.com>
2016-09-29 11:07:41 -0500

    Another fix for Emscripten environment

--- .travis/setenv.sh
+++ .travis/setenv.sh
@@ -2,14 +2,14 @@
 
 if [[ "$COMPILER" == "Emscripten" ]]; then
   CMAKE_TOOLCHAIN_FILE=`docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' thewtex/cross-compiler-browser-asmjs | grep CMAKE_TOOLCHAIN_FILE | sed 's/^.*=//'`
-  CONFIG_ARGS="-DBoost_INCLUDE_DIR=/usr/local/include -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE"
-  RUN="docker run --rm \
+  export CONFIG_ARGS="-DBoost_INCLUDE_DIR=/usr/local/include -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE"
+  export RUN="docker run --rm \
                   -v $PWD:/mfast \
                   -v /usr/include/boost:/usr/local/include/boost \
-                  -e "AR="
-                  -e "CXX="
-                  -w "/mfast"
+                  -e AR= \
+                  -e CXX= \
+                  -w /mfast \
                   -t thewtex/cross-compiler-browser-asmjs "
 else
-  CONFIG_ARGS="-DCMAKE_CXX_COMPILER=$COMPILER"
+  export CONFIG_ARGS="-DCMAKE_CXX_COMPILER=$COMPILER"
 fi                  
