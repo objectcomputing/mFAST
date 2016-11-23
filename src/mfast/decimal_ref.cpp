dcf19cb46e71fe0fb2edb8b178b0fce56b37bbde
Huang-Ming Huang <huangh@ociweb.com>
2016-11-23 11:17:29 -0600

    fixed link issue with boost 1.62

--- src/mfast/decimal_ref.cpp
+++ src/mfast/decimal_ref.cpp
@@ -12,3 +12,12 @@
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
 template class boost::multiprecision::backends::cpp_dec_float<18>;
+
+#if BOOST_VERSION >= 106200
+#include <boost/functional/hash.hpp>
+
+template void boost::hash_combine<boost::multiprecision::backends::cpp_dec_float<18u, int, void>::enum_fpclass_type>(unsigned long&, boost::multiprecision::backends::cpp_dec_float<18u, int, void>::enum_fpclass_type const&);
+template void boost::hash_combine<bool>(unsigned long&, bool const&);
+template void boost::hash_combine<int>(unsigned long&, int const&);
+template void boost::hash_combine<unsigned int>(unsigned long&, unsigned int const&);
+#endif
