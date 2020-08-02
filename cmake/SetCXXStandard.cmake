0e83af94e94d23ad7245ccdfcee5e5eb251fda3d
kan <sasi@jeppy.com>
2020-08-02 21:08:43 +0300

    Disable non-portable compiler extensions.

--- cmake/SetCXXStandard.cmake
+++ cmake/SetCXXStandard.cmake
@@ -10,5 +10,5 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 if (NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   ### Even though CMAKE_CXX_STANDARD_REQUIRED is supported since CMake 3.1, it doesn't work for Emscripten em++ together with
   ### Cmake 3.6.
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++${CMAKE_CXX_STANDARD}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++${CMAKE_CXX_STANDARD}")
 endif()
