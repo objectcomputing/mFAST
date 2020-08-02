34248c0d04ff32293b2a912ae1774a15270c9f6b
kan <sasi@jeppy.com>
2020-08-02 20:41:53 +0300

    Add check for boost version to set appropriate c++ standard.

--- cmake/SetCXXStandard.cmake
+++ cmake/SetCXXStandard.cmake
@@ -1,6 +1,11 @@
 
+find_package(Boost 1.56.0 REQUIRED)
+if(Boost_VERSION VERSION_LESS "1.72.0")
+	set(CMAKE_CXX_STANDARD 11 CACHE INTERNAL "specifies the C++ standard whose features are requested to build this target")
+else()
+	set(CMAKE_CXX_STANDARD 14 CACHE INTERNAL "specifies the C++ standard whose features are requested to build this target")
+endif()
 
-set(CMAKE_CXX_STANDARD 11 CACHE INTERNAL "specifies the C++ standard whose features are requested to build this target")
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 if (NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   ### Even though CMAKE_CXX_STANDARD_REQUIRED is supported since CMake 3.1, it doesn't work for Emscripten em++ together with
