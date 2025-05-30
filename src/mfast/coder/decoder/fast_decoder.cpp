c11bfa4217d84caace9a4ab570e3bca38586d5c9
Roberto Martin Fantini <martin.fantini@gmail.com>
2025-05-30 11:24:37 +0200

    Null dangling pointer for the case of pmap

--- src/mfast/coder/decoder/fast_decoder.cpp
+++ src/mfast/coder/decoder/fast_decoder.cpp
@@ -290,6 +290,7 @@ message_type *fast_decoder_impl::decode_segment(fast_istreambuf &sb) {
   for (auto &&field : message->ref()) {
     apply_mutator(*this, field);
   }
+  this->current_ = nullptr;
   return message;
 }
 
